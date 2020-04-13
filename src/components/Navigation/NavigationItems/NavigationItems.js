f670d8d60ecab4262a714bc82dbec8cdc5f8ff7b
machine <analyticrye@gmail.com>
2020-04-13 04:31:21 -0400

    toggle component added

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import classes from "./NavigationItems.module.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
-
 const NavigationItems = () => (
   <ul className={classes.NavigationItems}>
     <NavigationItem link="/" active>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,10 +3,11 @@ import React from "react";
 import classes from "./Toolbar.module.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToggle from "../../UI/DrawerToggle/DrawerToggle";
 
 const Toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <DrawerToggle toggle={props.toggleMenu} />
     <div className={classes.Logo}>
       <Logo />
     </div>
