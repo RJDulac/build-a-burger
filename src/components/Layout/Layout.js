1e2f80c118c60bd7506246ea0ad59baedd174102
machine <analyticrye@gmail.com>
2020-04-13 02:55:18 -0400

    sideDrawer component added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,11 @@ import React from "react";
 import Aux from "../../HOC/Aux";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
-
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 const Layout = props => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
