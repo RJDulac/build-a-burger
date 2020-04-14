6e6f144eaafc51f37acd6cb068af367e969daa27
machine <analyticrye@gmail.com>
2020-04-14 00:19:40 -0400

    optimized modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 
 import classes from "./Modal.module.css";
 import Aux from "../../../HOC/Aux";
 import Backdrop from "../Backdrop/Backdrop";
 
-const Modal = props => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      style={{
-        //my be better as a class
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0"
-      }}
-      className={classes.Modal}
-    >
-      {props.children}
-    </div>
-  </Aux>
-);
+class Modal extends Component {
+  //make sure OrderSummary doesn't update when it doesn't need to
+  //pure component has too much overhead for this case
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          style={{
+            //my be better as a class
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+          className={classes.Modal}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
 
 export default Modal;
